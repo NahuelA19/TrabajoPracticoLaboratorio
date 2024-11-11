4efd6fc906beb38e0fe2232b599621c466bc77bc
Nahuel Aciar <142058238+NahuelA19@users.noreply.github.com>
2024-11-11 13:53:38 -0300

    Update CargaNotas.java

--- Practica Relaciones/ejercicio1/CargaNotas.java
+++ Practica Relaciones/ejercicio1/CargaNotas.java
@@ -7,7 +7,7 @@ public class CargaNotas {
         ArrayList<Alumno> alumnos = new ArrayList<>();
 
 
-        // Pedir al usuario la cantidad de alumnos
+
         System.out.print("Ingrese la cantidad de alumnos: ");
         int cantidadAlumnos = scanner.nextInt();
 
@@ -15,16 +15,16 @@ public class CargaNotas {
             System.out.println("Alumno " + (i + 1));
 
 
-            // Crear un nuevo alumno
+    
             Alumno alumno = new Alumno();
 
-            // Pedir el nombre completo y legajo del alumno
+        
             System.out.print("Ingrese el nombre completo: ");
             alumno.setNombreCompleto(scanner.next());
             System.out.print("Ingrese el legajo: ");
             alumno.setLegajo(scanner.nextLong());
 
-            // Pedir la cantidad de notas
+      
             System.out.print("Ingrese la cantidad de notas: ");
             int cantidadNotas = scanner.nextInt();
 
@@ -34,14 +34,14 @@ public class CargaNotas {
                 cantidadNotas = scanner.nextInt();
             }
 
-            // Cargar las notas del alumno
+            
             for (int j = 0; j < cantidadNotas; j++) {
                 System.out.print("Ingrese la nota " + (j + 1) + ": ");
                 double nota = scanner.nextDouble();
                 alumno.agregarNota(new Nota("Materia " + (j + 1), nota));
             }
 
-            // Agregar el alumno a la lista
+           
             alumnos.add(alumno);
         }
 
